d1752dd02392aae5a52a421499125630cfcee960
kirti18291 <73463323+kirti18291@users.noreply.github.com>
2021-03-09 21:28:39 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,44 @@
+function imageEnlarge1() {
+ document.getElementById('1').style.height="30%";
+ document.getElementById('1').style.width="30%";
+ document.getElementById('1').style.position='relative';
+ }
+function imageReset1() {
+
+ document.getElementById('1').style.height="10%";
+ document.getElementById('1').style.width="10%";
+ document.getElementById('1').style.position='relative';
+ }
+ function imageEnlarge2() {
+ document.getElementById('2').style.height="30%";
+ document.getElementById('2').style.width="30%";
+ document.getElementById('2').style.position='relative';
+ }
+function imageReset2() {
+
+ document.getElementById('2').style.height="10%";
+ document.getElementById('2').style.width="10%";
+ document.getElementById('2').style.position='relative';
+ }
+ function imageEnlarge3() {
+ document.getElementById('3').style.height="30%";
+ document.getElementById('3').style.width="30%";
+ document.getElementById('3').style.position='relative';
+ }
+ function imageReset3() {
+
+ document.getElementById('3').style.height="10%";
+ document.getElementById('3').style.width="10%";
+ document.getElementById('3').style.position='relative';
+ }
+  function imageEnlarge4() {
+ document.getElementById('4').style.height="30%";
+ document.getElementById('4').style.width="30%";
+ document.getElementById('4').style.position='relative';
+ }
+function imageReset4() {
+
+ document.getElementById('4').style.height="10%";
+ document.getElementById('4').style.width="10%";
+ document.getElementById('4').style.position='relative';
+ }
